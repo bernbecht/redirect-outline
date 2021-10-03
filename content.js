840ccdb639ce56a51388fd26b2897514d23a434c
Bernardo B <1577809+Berhell@users.noreply.github.com>
2021-10-03 17:43:50 +0200

    Fix permission bug

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 function redirect() {
-  let currentUrl = window.location.href;
+  const currentUrl = window.location.href;
   const articleUrl = `https://outline.com/${currentUrl}`;
   window.open(articleUrl, "_blank").focus();
 }
